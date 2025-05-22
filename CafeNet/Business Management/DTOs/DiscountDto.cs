07819df0610c5b398e711713ee994d4fdb2202c2
Milda Domarkaitė <119532702+Domarkaite@users.noreply.github.com>
2025-05-22 21:18:41 +0300

    CN22 required code

--- CafeNet/Business Management/DTOs/DiscountDto.cs
+++ CafeNet/Business Management/DTOs/DiscountDto.cs
@@ -3,7 +3,7 @@
     public class DiscountDto
     {
         public long Id { get; set; }
-        public string Code { get; set; }
+        public required string Code { get; set; }
         public byte? Percent { get; set; }
         public decimal? Amount { get; set; }
     }
