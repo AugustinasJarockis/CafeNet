a821259b7ce160388eb0904fc1d4d1dad0fb178a
AldasKleinas <118926071+AldasKleinas@users.noreply.github.com>
2025-05-22 22:11:24 +0300

    CN24 fixed typo

--- CafeNet.Front/src/pages/menu-admin.tsx
+++ CafeNet.Front/src/pages/menu-admin.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from '@/components/app-sidebar';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,16 +6,15 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
+} from '@/components/ui/sidebar';
 
 export default function AdminMenu() {
-
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -23,7 +22,7 @@ export default function AdminMenu() {
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="verticaWl" className="mr-2 h-4" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
@@ -49,5 +48,5 @@ export default function AdminMenu() {
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
